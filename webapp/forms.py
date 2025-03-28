1d6a872ac46067d37ac50179a8dc7d54bece3c8a
Deyemi-kum <your-email@example.com>
2025-03-28 22:29:20 +0000

    update

--- webapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .itreporting.models import Student
-
-class StudentProfileForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ['photo', 'bio', 'group']
